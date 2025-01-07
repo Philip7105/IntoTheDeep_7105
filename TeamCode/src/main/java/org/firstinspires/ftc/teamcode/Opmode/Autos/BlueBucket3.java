1bf9c8a407576efafe28af5bfe2a552ab8d1b84d
Brady <brady.wortman@outlook.com>
2025-01-06 19:26:58 -0500

    Finished my BlueBucket3 auto, it just needs testing most likly will fail so be ready.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/Autos/BlueBucket3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/Autos/BlueBucket3.java
@@ -1,35 +1,61 @@
 package org.firstinspires.ftc.teamcode.Opmode.Autos;
 
+import static org.firstinspires.ftc.teamcode.robot.Subsystems.DepositingMechanisms.HorizontalSlides.HorizontalSlideStates.Fully_In;
+import static org.firstinspires.ftc.teamcode.robot.Subsystems.DepositingMechanisms.HorizontalSlides.fullin;
+import static org.firstinspires.ftc.teamcode.robot.Subsystems.DepositingMechanisms.VerticalSlides.autoTolerance;
 import static org.firstinspires.ftc.teamcode.robot.Subsystems.DepositingMechanisms.VerticalSlides.highbasket;
+import static org.firstinspires.ftc.teamcode.robot.Subsystems.DepositingMechanisms.VerticalSlides.lowbasket;
 import static org.firstinspires.ftc.teamcode.robot.Subsystems.DepositingMechanisms.VerticalSlides.lowchamber;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Rotation2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.CommandFrameWork.BaseAuto;
 import org.firstinspires.ftc.teamcode.CommandFrameWork.MultipleCommand;
+import org.firstinspires.ftc.teamcode.robot.Commands.ScoringCommands.SimpleCommands.Delay;
+import org.firstinspires.ftc.teamcode.robot.Commands.ScoringCommands.SimpleCommands.MultipleRRActionsWithPathing;
+import org.firstinspires.ftc.teamcode.robot.Subsystems.Intake.JohnsIntake;
 
 @Config
 @Autonomous
 public class BlueBucket3 extends BaseAuto {
     @Override
     public void runAuto() {
-// This is the starting position and heading for Blue Bucket 3 Auto
-        robot.driveTrain.setPoseEstimate(new Vector2d(32, 60), Rotation2d.exp(Math.toRadians(180)));
-        //This is the next postion for the robot it is to pick up the yellow sample
-//        runpath = new MultipleCommand(new StrafetoLinearHeading(new Vector2d(48.2,47),Rotation2d.exp(Math.toRadians(90)), lowchamber, robot, groups))
-                //This is the next potistion of the robot it is to deposit at the high basket with the yellow sample
-//        .addNext(new StrafetoLinearHeading(new Vector2d(57, 55),Rotation2d.exp(Math.toRadians(225)),highbasket,robot,groups))
-                //this is the next position the robot is to drive to the next yellow sample and pick it up
-//        .addNext(new StrafetoLinearHeading(new Vector2d(58.2, 47),Rotation2d.exp(Math.toRadians(270)),highbasket,robot,groups))
-                //This is the next potistion of the robot it is to deposit at the high basket with the yellow sample
-//        .addNext(new StrafetoLinearHeading(new Vector2d(57,55),Rotation2d.exp(Math.toRadians(225)),highbasket,robot,groups))
-                //this is the next potistion for the robot it is to pick up the yellow sample in a 45 ish degree angle
-//        .addNext(new StrafetoLinearHeading(new Vector2d(56.2, 47),Rotation2d.exp(Math.toRadians(305)),highbasket,robot,groups))
-                //This is the next potistion of the robot it is to deposit at the high basket with the yellow sample
-//        .addNext(new StrafetoLinearHeading(new Vector2d(57,47),Rotation2d.exp(Math.toRadians(225)),highbasket,robot,groups));
+        robot.driveTrain.setPoseEstimateBetter(new Vector2d(32, 60), Math.toRadians(180));
+        runpath = new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(32, 60), Math.toRadians(180),new Vector2d(48.2,47),Math.toRadians(90))})
+                .addNext(new Delay(2))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{groups.fullExtendHorizontalSLidesAction(),groups.moveIntakeAction(JohnsIntake.IntakeStates.intake),groups.movePivotAction(JohnsIntake.PivotStates.slightly_lower_pickup)}))
+                .addNext(new Delay(2))
+                .addNext(new MultipleCommand(groups.moveGripper(JohnsIntake.GripperStates.clamp),groups.moveIntake(JohnsIntake.IntakeStates.stop), groups.moveHorizontalSlides(Fully_In, fullin)))
+                .addNext(new MultipleCommand(groups.slidesSetPos(lowbasket), groups.movePivotJohn(JohnsIntake.PivotStates.basketpos)))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(48.2,47),Math.toRadians(90),new Vector2d(57, 55),Math.toRadians(225)),groups.moveVerticalSlidesAction(highbasket,autoTolerance),groups.moveGripperAction(JohnsIntake.GripperStates.unclamp),groups.moveIntakeAction(JohnsIntake.IntakeStates.outtake)}))
+                .addNext(new Delay(2))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(57, 55),Math.toRadians(225),new Vector2d(52.3, 56),Math.toRadians(225)),groups.moveIntakeAction(JohnsIntake.IntakeStates.stop),groups.moveVerticalSlidesAction(0,autoTolerance),groups.movePivotAction(JohnsIntake.PivotStates.parallel)}))
+                .addNext(new Delay(2))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(52.3, 56),Math.toRadians(225),new Vector2d(58.2, 47),Math.toRadians(270))}))
+                .addNext(new MultipleCommand(groups.fullExtendHorizontalSLides(),groups.moveIntake(JohnsIntake.IntakeStates.intake)))
+                .addNext(new MultipleCommand(groups.movePivotJohn(JohnsIntake.PivotStates.slightly_lower_pickup)))
+                .addNext(new Delay(2))
+                .addNext(new MultipleCommand(groups.movePivotJohn(JohnsIntake.PivotStates.parallel),groups.moveGripper(JohnsIntake.GripperStates.clamp),groups.moveIntake(JohnsIntake.IntakeStates.stop),groups.moveHorizontalSlides(Fully_In,fullin)))
+                .addNext(new Delay(1))
+                .addNext(new MultipleCommand(groups.slidesSetPos(lowbasket), groups.movePivotJohn(JohnsIntake.PivotStates.basketpos)))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(58.2, 47),Math.toRadians(270),new Vector2d(57, 55),Math.toRadians(225)),groups.moveVerticalSlidesAction(highbasket,autoTolerance),groups.moveGripperAction(JohnsIntake.GripperStates.unclamp),groups.moveIntakeAction(JohnsIntake.IntakeStates.outtake)}))
+                .addNext(new Delay(2))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(57, 55),Math.toRadians(225),new Vector2d(52.3, 56),Math.toRadians(225)),groups.moveIntakeAction(JohnsIntake.IntakeStates.stop),groups.moveVerticalSlidesAction(0,autoTolerance),groups.movePivotAction(JohnsIntake.PivotStates.parallel)}))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(52.3, 56),Math.toRadians(225),new Vector2d(56.2, 47),Math.toRadians(305))}))
+                .addNext(new MultipleCommand(groups.fullExtendHorizontalSLides(),groups.moveIntake(JohnsIntake.IntakeStates.intake),groups.movePivotJohn(JohnsIntake.PivotStates.slightly_lower_pickup)))
+                .addNext(new Delay(2))
+                .addNext(new MultipleCommand(groups.movePivotJohn(JohnsIntake.PivotStates.parallel),groups.moveGripper(JohnsIntake.GripperStates.clamp),groups.moveIntake(JohnsIntake.IntakeStates.stop),groups.moveHorizontalSlides(Fully_In,fullin)))
+                .addNext(new Delay(1))
+                .addNext(new MultipleCommand(groups.slidesSetPos(lowbasket), groups.movePivotJohn(JohnsIntake.PivotStates.basketpos)))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(56.2, 47),Math.toRadians(305), new Vector2d(57, 55),Math.toRadians(225)),groups.moveVerticalSlidesAction(highbasket,autoTolerance),groups.moveGripperAction(JohnsIntake.GripperStates.unclamp),groups.moveIntakeAction(JohnsIntake.IntakeStates.outtake)}))
+                .addNext(new Delay(2))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(57, 55),Math.toRadians(225),new Vector2d(52.3, 56),Math.toRadians(225)),groups.moveIntakeAction(JohnsIntake.IntakeStates.stop),groups.moveVerticalSlidesAction(0,autoTolerance),groups.movePivotAction(JohnsIntake.PivotStates.parallel)}))
+                .addNext(new MultipleRRActionsWithPathing(new Action[]{robot.driveTrain.strafeToLinearHeading(new Vector2d(52.3, 56),Math.toRadians(225),new Vector2d(24,0),Math.toRadians(180))}));
+
 
 
 
